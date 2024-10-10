0bb1c31bd6907a5d5b6378c28cbbdacee58f7c45
unknown <ibanezlucas@chaco.gob.ar>
2024-10-10 15:53:27 -0300

    primeras vistas

--- vacunacion/vacunacion/urls.py
+++ vacunacion/vacunacion/urls.py
@@ -6,5 +6,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('inicio/', views.mi_pagina_inicio)
+    path('inicio/', views.mi_pagina_inicio, name="inicio"),
+    path('usuarios/lista/', views.lista_usuarios, name="lista_de_usuarios"),
+    path('pacientes/lista/', views.lista_pacientes, name="lista_de_pacientes")
 ]
--- vacunacion/vacunacion/views.py
+++ vacunacion/vacunacion/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
 def mi_pagina_inicio(request):
-    contexto = {}
+    lista_usuarios = [
+        {"nombre": "Lucas", "apellido": "Ibañez"},
+        {"nombre": "Federico", "apellido": "Aguirre"},
+        {"nombre": "Raul", "apellido": "Rolon"},
+    ]
+    contexto = {
+        "todo_los_usuarios": lista_usuarios,
+        "usuario_autenticado": "Lucas Ibañez"
+    }
     return render(request, 'mi_pagina_inicio.html', contexto)
+
+def lista_usuarios(request):
+    return render(request, 'lista_usuarios.html', {})
+
+def lista_pacientes(request):
+    return render(request, 'lista_pacientes.html', {})
