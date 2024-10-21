8eff52023319cba7611ed1d7c1d4715efd15a4a2
unknown <ibanezlucas@chaco.gob.ar>
2024-10-21 15:03:41 -0300

    list users

--- src/apps/usuarios/models.py
+++ src/apps/usuarios/models.py
@@ -4,3 +4,6 @@ from django.contrib.auth.models import AbstractUser
 
 class Usuario(AbstractUser):
     dni = models.IntegerField(null=True)
+
+    def __str__(self):
+        return f"{self.id} - {self.username}. {self.first_name} - {self.last_name}"
--- src/apps/usuarios/views.py
+++ src/apps/usuarios/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
 
+from .models import Usuario
+
 def lista_usuarios(request):
-    return render(request, 'lista_usuarios.html', {})
+    usuarios = Usuario.objects.all() # filter(id__in=[1,2,3])#
+    """
+    u = Usuario.objects.filter(id=1).first()
+    # u = Usuario.objects.get(id=5)
+    print("TODOS LOS USUARIOS")
+    if u is None:
+        print("no existe")
+    else:
+
+        print(u)
+        # print(u.query)
+        #u = u[0]
+        print(u.username)
+    
+    print("long:", len(usuarios))
+    print("long:", usuarios.count())
+    for us in usuarios:
+        print(us.last_login)
+    """
+    ctx = {
+        "usuarios": usuarios
+    }
+    return render(request, 'usuarios/lista.html', ctx)
