1e7edfc9833fb2e6358e6a1862183ea6c5c68227
unknown <ibanezlucas@chaco.gob.ar>
2024-10-28 15:38:36 -0300

    .

--- src/apps/pacientes/urls.py
+++ src/apps/pacientes/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = "pacientes"
 
 urlpatterns = [
-    path('lista/', views.lista, name="lista"),
-    path('nuevo/', views.nuevo, name="nuevo"),
+    path('lista/', views.Lista.as_view(), name="lista"),
+    # path('lista/', views.lista, name="lista"),
+    # path('nuevo/', views.nuevo, name="nuevo"),
+    path('nuevo/', views.Nuevo.as_view(), name="nuevo"),
 ]
--- src/apps/pacientes/views.py
+++ src/apps/pacientes/views.py
@@ -1,10 +1,49 @@
 from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
+from django.urls import reverse_lazy
 
+from .models import Paciente
 from .forms import FormPaciente
 
-
+"""
 def lista(request):
     return render(request, 'pacientes/lista.html', {})
+"""
+
+class Lista(ListView):
+    template_name = 'pacientes/lista.html'
+    model = Paciente
+    context_object_name = "pacientes"
+    paginate_by = 1
+
+    def get_context_data(self, **kwargs):
+        ctx = super(Lista, self).get_context_data(**kwargs)
+        ctx["titulo"] = "LISTA DE PACIENTES"
+        return ctx
+    
+    def get_queryset(self):
+        query = self.model.objects.all()
+        print(self.request.method)
+        nombre = self.request.GET.get('nombre', None)
+        print("nombre", nombre)
+        if nombre:
+            query = query.filter(nombre=nombre)
+        print("query", query)
+        return query.order_by("apellido")
+
+class Nuevo(CreateView):
+    template_name = 'pacientes/nuevo.html'
+    model = Paciente
+    form_class = FormPaciente
+    success_url = reverse_lazy("pacientes:lista")
+
+    def get_context_data(self, **kwargs):
+        ctx = super(Nuevo, self).get_context_data(**kwargs)
+        ctx["titulo"] = "NUEVO PACIENTE"
+        return ctx
+
+"""
 
 def nuevo(request):
     template_name = 'pacientes/nuevo.html'
@@ -24,3 +63,4 @@ def nuevo(request):
         "message": message
     }
     return render(request, template_name, ctx) 
+"""
