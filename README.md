7372e073c3c0420d4fc5131b792072abbe13ca42
Fede A <federodani@gmail.com>
2024-10-31 15:43:01 -0300

    utilizando envirion

--- README.md
+++ README.md
@@ -11,5 +11,10 @@ Funcionalidades
 * Trunos (?)
 
 
+### Despliegue
+1) Crear Entorno virtual
+2) Instalar dependencias del requirements/base.txt
+3) crear un archivo ".env" con los datos y estructuras de "env_example"
+
 ### Python
 Versión 3.12.6
--- src/config/settings.py
+++ src/config/settings.py
@@ -1,19 +1,38 @@
+import environ
+import os
 from pathlib import Path
 from django.urls import reverse_lazy
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Apartado configuración con variables de entornos
+env = environ.Env()
+env_file = os.path.join(os.path.dirname(BASE_DIR), ".env")
+
+env.read_env(env_file=env_file, overwrite=True)
+
+SECRET_KEY_DEFAULT = 'django-insecure-ixnzv*k#2j8g6@y&iq!bjg_l188nh0b-y@v8vg(mv8hqx@+@uv'
+
+SECRET_KEY = env("SECRET_KEY", default=SECRET_KEY_DEFAULT)
+
+DEBUG = env.bool('DJANGO_DEBUG', default=True)
+
+ENVIRONMENT_RUN = env("ENVIRONMENT_RUN", default="local")
+
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=['*'])
+# fin apartado entorno
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ixnzv*k#2j8g6@y&iq!bjg_l188nh0b-y@v8vg(mv8hqx@+@uv'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-ixnzv*k#2j8g6@y&iq!bjg_l188nh0b-y@v8vg(mv8hqx@+@uv'
+#
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+#
+# ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -67,16 +86,35 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+DEFAULT_DB_NAME = env("POSTGRES_DB")
+DEFAULT_DB_USER = env("POSTGRES_USER")
+DEFAULT_DB_HOST = env("POSTGRES_HOST")
+DEFAULT_DB_PORT = env("POSTGRES_PORT")
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'app_vacunacion',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '5432',  
+        'NAME': DEFAULT_DB_NAME,
+        'USER': DEFAULT_DB_USER,
+        'PASSWORD': env("POSTGRES_PASSWORD"),
+        'HOST': DEFAULT_DB_HOST,
+        'PORT': DEFAULT_DB_PORT,
+        'ATOMIC_REQUESTS': True,
+        'CONN_MAX_AGE': env.int("CONN_MAX_AGE", default=60)
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'app_vacunacion',
+#         'USER': 'postgres',
+#         'PASSWORD': '123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 
